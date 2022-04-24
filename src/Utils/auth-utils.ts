f409c1d19c9fa010ea0f5aa2213ac02aeed1effe
Farolan Faisal <farolanfaisal@gmail.com>
2022-04-24 18:15:50 +0800

    feat: save auth to redis

--- src/Utils/auth-utils.ts
+++ src/Utils/auth-utils.ts
@@ -179,3 +179,61 @@ export const useSingleFileAuthState = (filename: string, logger?: Logger): { sta
 		saveState
 	}
 }
+
+export const fetchAuthState = async(key: String, redis: any, logger?: Logger): Promise<{ state: AuthenticationState, saveState: () => void }> => {
+	let creds: AuthenticationCreds
+	let keys: any = { }
+
+	// save the authentication state to a file
+	const saveState = async () => {
+		logger && logger.trace('saving auth state')
+		// BufferJSON replacer utility saves buffers nicely
+		const value = JSON.stringify({ creds, keys }, BufferJSON.replacer, 2)
+		await redis.set(key, value)
+	}
+
+	const value = await redis.get(key)
+	if(value) {
+		const result = JSON.parse(value, BufferJSON.reviver)
+		creds = result.creds
+		keys = result.keys
+	} else {
+		creds = initAuthCreds()
+		keys = { }
+	}
+
+	return {
+		state: {
+			creds,
+			keys: {
+				get: (type, ids) => {
+					const key = KEY_MAP[type]
+					return ids.reduce(
+						(dict, id) => {
+							let value = keys[key]?.[id]
+							if(value) {
+								if(type === 'app-state-sync-key') {
+									value = proto.AppStateSyncKeyData.fromObject(value)
+								}
+
+								dict[id] = value
+							}
+
+							return dict
+						}, { }
+					)
+				},
+				set: (data) => {
+					for(const _key in data) {
+						const key = KEY_MAP[_key as keyof SignalDataTypeMap]
+						keys[key] = keys[key] || { }
+						Object.assign(keys[key], data[_key])
+					}
+
+					saveState()
+				}
+			}
+		},
+		saveState
+	}
+}
